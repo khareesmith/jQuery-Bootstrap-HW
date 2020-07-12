80c6f37167656ec3db0dd7dab2da952fb24bdc1c
khareesmith <kharee.smith@yahoo.com>
2020-07-12 17:09:57 -0400

    added click functions to JS with log for buttons

---
+++ js/script.js
@@ -0,0 +1,43 @@
+var colors = ["red","orange","yellow","green","blue","purple","brown", "chocolate","darkgreen", "deeppink", "lightseagreen", "navy", "steelblue", "lime", "tomato", "violet"];
+var borders = ["solid","dotted","dashed","double","inset"];
+
+
+$(".changeBtnOne").click(function() {
+    console.log("click 1");
+});
+
+$(".changeBtnTwo").click(function() {
+    console.log("click 2");
+});
+
+$(".changeBtnThree").click(function() {
+    console.log("click 3");
+});
+
+$(".changeAllBtns").click(function() {
+    console.log("click all");
+});
+
+$(".hideBtnOne").click(function() {
+    console.log("Hide 1");
+});
+
+$(".hideBtnTwo").click(function() {
+    console.log("Hide 2");
+});
+
+$(".hideBtnThree").click(function() {
+    console.log("Hide 3");
+});
+
+$(".hideAllBtns").click(function() {
+    console.log("Hide All");
+});
+
+$(".resetBtn").click(function() {
+    console.log("RESET");
+});
+
+$(".showBtn").click(function() {
+    console.log("SHOW");
+});
